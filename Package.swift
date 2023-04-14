65c56894a001599f13babc357f37213b96c33d8a
mobileappconsultant <mobileappconzultant@gmail.com>
2023-04-14 20:58:32 +0100

    Updated new library version to unspecified

---
+++ Package.swift
@@ -0,0 +1,28 @@
+
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "lostintravelsdk",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "lostintravelsdk",
+            targets: ["lostintravelsdk"])
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "lostintravelsdk",
+            url: "https://github.com/mobileappconsultant/lostintravelsharedlibrary/raw/artifacts/lostintravelsdk.xcframework.zip",
+            checksum: "24d20481d711d744be5ed67eb2d4f13f262158615feff4a7596e13ed89d4a01f"
+        ),
+    ]
+)
